3712b29a1cdf18e30c09257f83f9139c09758339
Hector-Hernandez-Eslaba <157669650+Hector-Hernandez-Eslaba@users.noreply.github.com>
2024-06-02 23:11:28 -0600

    Add files via upload

---
+++ eliminarProducto.js
@@ -0,0 +1,23 @@
+import { conexionAPI } from "./conexionAPI.js";
+
+console.log("Se subió el archivo borrar Producto");
+
+async function eliminarProducto(id) {
+    await conexionAPI.borrarProducto(id);
+    window.location.href = "/index.html";
+    
+}
+
+window.addEventListener("load", function(){
+    const eliminar = document.querySelectorAll(".btn_Eliminar");
+
+    eliminar.forEach (btn => {
+        const id = btn.getAttribute("data-id");
+        console.log("ID del boton:" + id);
+
+        btn.addEventListener ("click", (event) => {
+            event.preventDefault();
+            eliminarProducto(id);
+        })
+    })
+})
